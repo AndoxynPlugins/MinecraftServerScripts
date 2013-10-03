ecc96d6b659404bd876018ca2914d63b8027a54b
Dabo Ross <daboross@daboross.net>
2013-10-03 12:32:54 -0700

    Does this work?

--- test.sh
+++
@@ -1,3 +0,0 @@
-rm -f failed
-find src -type f -exec bash -c 'file={};echo "- $file"; bash -n $file; if [[ $? != 0 ]]; then echo -n "$file " >> failed; fi' /. \;
-if [[ -a failed ]]; then echo "File(s) failed: `cat failed`"; rm -f failed; exit 1; else echo "All success!"; exit 0; fi
